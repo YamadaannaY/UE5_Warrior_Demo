046725593a1df0b2db57b54e23c45481cfcf1f2f
Yu <jiangjyjiang@gmail.com>
2025-10-07 18:35:58 +0800

    解决了UBTService_OrientToTargetActor每次打开编辑器都要livecoding并重新拼接的问题

--- warrior/warrior.Build.cs
+++ warrior/warrior.Build.cs
@@ -10,7 +10,7 @@ public class warrior : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput","GameplayTags","GameplayTasks","AnimGraphRuntime","AIModule"});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] {"AIModule"});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
