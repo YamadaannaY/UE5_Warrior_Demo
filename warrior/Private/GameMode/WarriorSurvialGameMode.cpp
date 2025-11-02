1f39d8134c158ad76b0d60e96ac8c11c9f380ddd
Yu <jiangjyjiang@gmail.com>
2025-11-02 17:37:32 +0800

    GameMode实现 1.设置具体的游戏玩法规则，对波次逻辑进行具体设计 2.实现一个委托接口，将各个State对应的逻辑在蓝图中实现 3.对软引用EnemyClass进行预加载，确定其生成位置并在特定状态下生成

---
+++ warrior/Private/GameMode/WarriorSurvialGameMode.cpp
@@ -0,0 +1,222 @@
+// Yu
+
+
+#include "GameMode/WarriorSurvialGameMode.h"
+#include "Characters/WarriorEnemyCharacter.h"
+#include "Engine/AssetManager.h"
+#include "Engine/TargetPoint.h"
+#include "Kismet/GameplayStatics.h"
+#include "NavigationSystem.h"
+
+/** GameMode流程： 在最开始为WaitSpawnNewWave状态，异步加载下一波次对象，然后进入波次间隔时间，此时为SpawningNewWave，再进入敌方单位生成时间，
+ *  此时InProgress，进行加载对象的实例化。再是等待时间InProgress，完成所有后进入WaveCompleted，这里有一段缓冲时间，用来加载下一波次敌方单位。		**/
+
+void AWarriorSurvivalGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	checkf(EnemyWaveSpawnerDataTable,TEXT("Forgot to assign a data table in survival game mode blueprint"));
+
+	//初始状态为等待新波次生成
+	SetCurrentGameModeState(EWarriorSurvivalGameModeState::WaitSpawnNewWave);
+	//获取Waves数量并进行存储，通过获取DT编辑器中配置Row的数量实现
+	TotalWavesToSpawn=EnemyWaveSpawnerDataTable->GetRowNames().Num();
+	
+	//每次都在WaitSpawnNewWave状态预加载下一波次的Class异步加载下一波次的敌方单位
+	PreLoadNextWaveSpawnEnemies();
+}
+
+//每一帧都对State进行判断
+void AWarriorSurvivalGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	//在WaitSpawnNewWave状态下读秒SpawnNewWavesWaitTime
+	if (CurrentSurvivalGameModeState==EWarriorSurvivalGameModeState::WaitSpawnNewWave)
+	{
+		TimePassedSinceStart+=DeltaTime;
+		//读秒完毕后，从WaitSpawnNewWave进入SpawningNewWave
+		if (TimePassedSinceStart>=SpawnNewWavesWaitTime)
+		{
+			TimePassedSinceStart=0.f;
+			SetCurrentGameModeState(EWarriorSurvivalGameModeState::SpawningNewWave);
+		}
+	}
+
+	//读秒SpawnEnemiesDelayTime，随后生成预加载好的敌方单位，进入InProgress状态
+	if (CurrentSurvivalGameModeState==EWarriorSurvivalGameModeState::SpawningNewWave)
+	{
+		TimePassedSinceStart+=DeltaTime;
+
+		if (TimePassedSinceStart>=SpawnEnemiesDelayTime)
+		{
+			CurrentSpawnEnemiesCounter+=TrySpawnWaveEnemies();
+			TimePassedSinceStart=0.f;
+			SetCurrentGameModeState(EWarriorSurvivalGameModeState::InProgress);
+		}
+	}
+	//当前波次所有EnemyClass生成完毕后，进入WaveCompleted状态，有一段空白时间作为缓冲
+	if (CurrentSurvivalGameModeState==EWarriorSurvivalGameModeState::WaveCompleted)
+	{
+		TimePassedSinceStart+=DeltaTime;
+		if (TimePassedSinceStart>=WaveCompletedWaitTime)
+		{
+			TimePassedSinceStart=0.f;
+			CurrentWavesCount++;
+			
+			//判断是否完成所有波次，是则AllWaveDone，否则WaitSpawnNewWave，进入下一波次循环
+			if (HasFinishedAllWaves())
+			{
+				SetCurrentGameModeState(EWarriorSurvivalGameModeState::AllWaveDone);
+			}
+			else
+			{
+				SetCurrentGameModeState(EWarriorSurvivalGameModeState::WaitSpawnNewWave);
+				PreLoadNextWaveSpawnEnemies();
+			}
+		}
+	}
+}
+
+void AWarriorSurvivalGameMode::SetCurrentGameModeState(EWarriorSurvivalGameModeState InState)
+{
+	CurrentSurvivalGameModeState = InState;
+	OnSurvivalGameModeStateChanged.Broadcast(CurrentSurvivalGameModeState);
+}
+
+bool AWarriorSurvivalGameMode::HasFinishedAllWaves() const
+{
+	//这个判断在自增操作之后，所以实际CurrentWavesCount会比真正参与判断的当前波次数大1
+	return CurrentWavesCount>TotalWavesToSpawn;
+}
+
+void AWarriorSurvivalGameMode::PreLoadNextWaveSpawnEnemies()
+{
+	if (HasFinishedAllWaves())
+	{
+		return;
+	}
+	//每次预加载必须首先保证数组是空的
+	PreLoadedEnemyClassMap.Empty();
+	
+	//遍历当前轮次的EnemyWaveSpawnerDefinitions
+	for (const FWarriorEnemyWaveSpawnerInfo& SpawnerInfo:GetCurrentWaveSpawnerTableRow()->EnemyWaveSpawnerDefinitions)
+	{
+		if (SpawnerInfo.SoftEnemyClassToSpawn.IsNull()) continue;
+		
+		//采用异步加载并存储加载结果
+		UAssetManager::GetStreamableManager().RequestAsyncLoad(SpawnerInfo.SoftEnemyClassToSpawn.ToSoftObjectPath(),FStreamableDelegate::CreateLambda([SpawnerInfo,this]()
+		{
+			if (UClass* LoadedEnemyClass=SpawnerInfo.SoftEnemyClassToSpawn.Get())
+			{
+				PreLoadedEnemyClassMap.Emplace(SpawnerInfo.SoftEnemyClassToSpawn,LoadedEnemyClass);
+			}
+		}));
+	}
+
+	
+}
+
+
+FWarriorEnemyWaveSpawnerTableRow* AWarriorSurvivalGameMode::GetCurrentWaveSpawnerTableRow() const
+{
+	//固定的Row头名的格式eg：Wave1 etc..
+	const FName RowName=FName(TEXT("Wave")+FString::FromInt(CurrentWavesCount));
+	//获取Row下数据
+	FWarriorEnemyWaveSpawnerTableRow* FoundRow=EnemyWaveSpawnerDataTable->FindRow<FWarriorEnemyWaveSpawnerTableRow>(RowName,FString());
+
+	checkf(FoundRow,TEXT("could not find the valid row under the name %s in the data table "),*RowName.ToString());
+
+	return FoundRow;
+}
+
+int32 AWarriorSurvivalGameMode::TrySpawnWaveEnemies()
+{
+	if (TargetPointsArray.IsEmpty())
+	{
+		UGameplayStatics::GetAllActorsOfClass(this,ATargetPoint::StaticClass(),TargetPointsArray);
+	}
+
+	checkf(!TargetPointsArray.IsEmpty(),TEXT("no valid target point found in level:%s for spawning enemies"),*GetWorld()->GetName());
+
+	uint32 EnemiesSpawnedThisTime =0;
+ 
+	//制定了生成对象时的碰撞处理方式参数
+	FActorSpawnParameters SpawnParam;
+	SpawnParam.SpawnCollisionHandlingOverride=ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	for (const FWarriorEnemyWaveSpawnerInfo SpawnerInfo : GetCurrentWaveSpawnerTableRow()->EnemyWaveSpawnerDefinitions)
+	{
+		if (SpawnerInfo.SoftEnemyClassToSpawn.IsNull()) continue;
+		//此次SpawnEnemy的个数
+		const int32 NumToSpawn=FMath::RandRange(SpawnerInfo.MinPerSpawnCount,SpawnerInfo.MaxPerSpawnCount);
+		
+		//找到对应加载好的UClass对象
+		UClass* LoadedEnemyClass=PreLoadedEnemyClassMap.FindChecked(SpawnerInfo.SoftEnemyClassToSpawn);
+
+		for (int32 i=0;i<NumToSpawn;i++)
+		{
+			//随机找一个位置
+			const int32 RandomTargetPointIndex=FMath::RandRange(0,TargetPointsArray.Num()-1);
+			const FVector SpawnOrigin=TargetPointsArray[RandomTargetPointIndex]->GetActorLocation();
+			//只返回TargetPoint前向向量的纯方向旋转，其他两个分量取默认值
+			const FRotator SpawnRotation=TargetPointsArray[RandomTargetPointIndex]->GetActorForwardVector().ToOrientationRotator();
+			//将这个生成位置进一步随机化
+			FVector RandomLocation;
+			UNavigationSystemV1::K2_GetRandomLocationInNavigableRadius(this,SpawnOrigin,RandomLocation,400.f);
+			//将生成位置抬高，避免陷入地面
+			RandomLocation+=FVector(0.f,0.f,150.f);
+			
+			//根据位置和旋转、UClass信息生成Enemy
+			AWarriorEnemyCharacter* SpawnEnemy=GetWorld()->SpawnActor<AWarriorEnemyCharacter>(LoadedEnemyClass,RandomLocation,SpawnRotation,SpawnParam);
+
+			//当Enemy被击杀时，回调OnEnemyDestroyed
+			if (SpawnEnemy)
+			{
+				SpawnEnemy->OnDestroyed.AddUniqueDynamic(this,&ThisClass::OnEnemyDestroyed);
+				EnemiesSpawnedThisTime++;
+				TotalSpawnEnemiesThisWaveCounter++;
+			}
+			if (!ShouldKeepSpawnEnemies())
+			{
+				return EnemiesSpawnedThisTime;
+			}
+		}
+		
+	}
+	return EnemiesSpawnedThisTime;;
+}
+
+bool AWarriorSurvivalGameMode::ShouldKeepSpawnEnemies() const
+{
+	return TotalSpawnEnemiesThisWaveCounter < GetCurrentWaveSpawnerTableRow()->TotalEnemyToSpawnThisWave;
+}
+
+void AWarriorSurvivalGameMode::OnEnemyDestroyed(AActor* DestroyedActor)
+{
+	CurrentSpawnEnemiesCounter--;
+
+	/*Debug::Print(FString::Printf(TEXT("CurrentSpawnEnemiesCounter=%i,TotalSpawnedEnemiesThisWaveCounter= %i"),CurrentSpawnEnemiesCounter,TotalSpawnEnemiesThisWaveCounter)); */
+	if (ShouldKeepSpawnEnemies())
+	{
+		CurrentSpawnEnemiesCounter+=TrySpawnWaveEnemies();
+	}
+	
+	//如果判断不继续生成敌方单位且当前关卡内敌方单位数量为0，意味着波次完成
+	else if (CurrentSpawnEnemiesCounter==0)
+	{
+		TotalSpawnEnemiesThisWaveCounter=0;
+		SetCurrentGameModeState(EWarriorSurvivalGameModeState::WaveCompleted);
+	}
+}
+
+void AWarriorSurvivalGameMode::RegisterSpawnedEnemies(const TArray<AWarriorEnemyCharacter*>& InEnemiesToRegister)
+{
+	for (AWarriorEnemyCharacter* SpawnedEnemy : InEnemiesToRegister)
+	{
+		if (SpawnedEnemy)
+		{
+			CurrentSpawnEnemiesCounter++;
+			
+			SpawnedEnemy->OnDestroyed.AddUniqueDynamic(this,&ThisClass::OnEnemyDestroyed);
+		}
+	}
+}
---
+++ warrior/Public/GameMode/WarriorSurvialGameMode.h
@@ -0,0 +1,154 @@
+// Yu
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameMode/WarriorGameMode.h"
+#include "WarriorSurvialGameMode.generated.h"
+
+class AWarriorEnemyCharacter;
+
+UENUM(BlueprintType)
+enum class EWarriorSurvivalGameModeState:uint8
+{
+	WaitSpawnNewWave,
+	SpawningNewWave,
+	InProgress,
+	WaveCompleted,
+	AllWaveDone,
+	PlayerDied,
+};
+
+USTRUCT(BlueprintType)
+struct FWarriorEnemyWaveSpawnerInfo
+{
+	GENERATED_BODY()
+
+	//软引用,在新波次之前异步加载
+	UPROPERTY(EditAnywhere)
+	TSoftClassPtr<AWarriorEnemyCharacter> SoftEnemyClassToSpawn;
+
+	//生成EnemyClass的最小数量
+	UPROPERTY(EditAnywhere)
+	int8 MinPerSpawnCount=1;
+	
+	//生成EnemyClass的最大数量
+	UPROPERTY(EditAnywhere)
+	int8 MaxPerSpawnCount=3;
+};
+
+//实现Mode DT的内容行
+USTRUCT(BlueprintType)
+struct FWarriorEnemyWaveSpawnerTableRow:	public FTableRowBase
+{
+	GENERATED_BODY()
+
+	//存储单波次内的所有EnemyClass类型及其配置信息
+	UPROPERTY(EditAnywhere)
+	TArray<FWarriorEnemyWaveSpawnerInfo> EnemyWaveSpawnerDefinitions;
+
+	//此波次内一共要生成多少Enemy
+	UPROPERTY(EditAnywhere)
+	int32 TotalEnemyToSpawnThisWave=1;
+	
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSurvivalGameModeStateChanged,EWarriorSurvivalGameModeState,CurrentState);
+
+/**
+ * 
+ */
+
+UCLASS()
+class WARRIOR_API AWarriorSurvivalGameMode : public AWarriorGameMode
+{
+	GENERATED_BODY()
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	/**内部使用函数 **/
+	
+	//设置当前State
+	void SetCurrentGameModeState(EWarriorSurvivalGameModeState InState);
+	
+	//判断是否所有波次结束
+	bool HasFinishedAllWaves() const ;
+	
+	//异步加载下一波次的敌方单位
+	void PreLoadNextWaveSpawnEnemies();
+	
+	//通过RowName检索当前波次对应的DT行，获取具体配置信息
+	FWarriorEnemyWaveSpawnerTableRow* GetCurrentWaveSpawnerTableRow() const;
+	
+	//将加载好的敌方单位在指定位置进行生成
+	int32 TrySpawnWaveEnemies();
+	
+	//是否继续生成，判断是否达到波次生成地方单位数量要求
+	bool ShouldKeepSpawnEnemies() const;
+	
+	//用于在当前波次内敌人全部被消灭且此波次要生成的总敌方单位数还未达标时调用
+	UFUNCTION()
+	void OnEnemyDestroyed(AActor* DestroyedActor);
+	/**内部使用函数 **/
+	
+	//当前State
+	UPROPERTY()
+	EWarriorSurvivalGameModeState CurrentSurvivalGameModeState;
+
+	//蓝图绑定委托，此时广播对应改变的State需要进行的逻辑将回调执行
+	UPROPERTY(BlueprintAssignable,BlueprintCallable)
+	FOnSurvivalGameModeStateChanged OnSurvivalGameModeStateChanged;
+
+	//DT对象，在DT实例中配置各波次的EnemyClass、TotalCount等等
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="WaveDefinition",meta=(AllowPrivateAccess=true))
+	UDataTable* EnemyWaveSpawnerDataTable;
+
+	//总波次
+	UPROPERTY(VisibleDefaultsOnly,BlueprintReadOnly,Category="WaveDefinition",meta=(AllowPrivateAccess=true))
+	int32 TotalWavesToSpawn;
+
+	//当前波次计数
+	UPROPERTY(VisibleDefaultsOnly,BlueprintReadOnly,Category="WaveDefinition",meta=(AllowPrivateAccess=true))
+	int32 CurrentWavesCount=1;
+
+	//当前存在的敌方单位个数
+	UPROPERTY()
+	int32 CurrentSpawnEnemiesCounter=0;
+
+	//此波次总共生成的敌方单位
+	UPROPERTY()
+	int32 TotalSpawnEnemiesThisWaveCounter=0;
+
+	//存储了所有的TargetPoint Actor,在其位置上进行敌方单位生成
+	UPROPERTY()
+	TArray<AActor*> TargetPointsArray;
+
+	//判断间隔时间
+	UPROPERTY()
+	float TimePassedSinceStart=0.f;
+
+	//等待时间之后波次间隔时间，有UI倒计时
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="WaveDefinition",meta=(AllowPrivateAccess=true))
+	float SpawnNewWavesWaitTime=5.f;
+
+	//波次开始后生成Enemy的间隔时间
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,Category="WaveDefinition",meta=(AllowPrivateAccess=true))
+	float SpawnEnemiesDelayTime=2.f;
+
+	//一波次之后的间隔时间，用来预加载EnemyClass，添加波次计数，给玩家进行缓冲
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="WaveDefinition",meta=(AllowPrivateAccess=true))
+	float WaveCompletedWaitTime=5.f;
+
+	//存储加载的角色和其软引用的映射Map
+	UPROPERTY()
+	TMap<TSoftClassPtr<AWarriorEnemyCharacter>,UClass*> PreLoadedEnemyClassMap;
+
+public:
+	//将Boss生成的敌方单位个数加入TotalSpawnEnemiesThisWaveCounter，赋予其调用OnEnemyDestroyed的能力
+	UFUNCTION(BlueprintCallable)
+	void RegisterSpawnedEnemies(const TArray<AWarriorEnemyCharacter*>& InEnemiesToRegister);
+	
+};
