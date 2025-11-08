355d82a1fd1151dc9fa8a5b499f1d3dcba7ce703
Yu <jiangjyjiang@gmail.com>
2025-11-08 17:17:54 +0800

    将HitReact方向Tag计算集合到GA_HitReact

--- warrior/Private/AbilitySystem/Abilities/HeroGameplayAbilit_HitReact.cpp
+++ warrior/Private/AbilitySystem/Abilities/HeroGameplayAbilit_HitReact.cpp
@@ -6,10 +6,11 @@
 #include "WarriorFunctionLibrary.h"
 #include "WarriorGamePlayTags.h"
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UHeroGameplayAbility_HitReact::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-                                                   const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
-                                                   const FGameplayEventData* TriggerEventData)
+                                                    const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                                    const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
@@ -81,4 +82,49 @@ void UHeroGameplayAbility_HitReact::SelectMontageAndPlay( const FGameplayEventDa
 		SkeletalMeshComponent->SetScalarParameterValueOnMaterials(TEXT("HitFxSwitch"),1);
 	}
 }
+
+FGameplayTag UHeroGameplayAbility_HitReact::ComputeHitReactDirectionTag(const AActor* InAttacker, AActor* InVictim,
+	float& OutAngleDifference)
+{
+	check(InAttacker && InVictim);
+	
+	//获取前向向量和二者之间的单位方向向量
+	const FVector VictimForward=InVictim->GetActorForwardVector();
+	const FVector VictimToAttackerNormalized=(InAttacker->GetActorLocation()-InVictim->GetActorLocation()).GetSafeNormal();
+
+	//计算弧度值并转化为角度
+	const float DotResult=FVector::DotProduct(VictimForward,VictimToAttackerNormalized);
+	OutAngleDifference=UKismetMathLibrary::DegAcos(DotResult);
+	
+	//获得叉积
+	const FVector CrossResult=FVector::CrossProduct(VictimForward,VictimToAttackerNormalized);
+	//点积的z轴决定了角度值的正负（根据点积右手定则，垂直向量的方向正负就是二者角度的正负取值）
+	if (CrossResult.Z<0.f)
+	{
+		OutAngleDifference*=-1.f;
+	}
+	//前方
+	if (OutAngleDifference>=-45.f && OutAngleDifference<=45.f)
+	{
+		return WarriorGamePlayTags::Shared_Status_HitReact_Front;
+	}
+	//右方
+	if (OutAngleDifference>45.f && OutAngleDifference<=135.f)
+	{
+		return WarriorGamePlayTags::Shared_Status_HitReact_Right;
+	}
+	//左方
+	if (OutAngleDifference>135.f || OutAngleDifference<=-135.f)
+	{
+		return WarriorGamePlayTags::Shared_Status_HitReact_Back;
+	}
+	//后方
+	if (OutAngleDifference<-45.f && OutAngleDifference>-135.f)
+	{
+		return WarriorGamePlayTags::Shared_Status_HitReact_Left;
+	}
+
+	return WarriorGamePlayTags::Shared_Status_HitReact_Front;
+}
+
 
--- warrior/Private/WarriorFunctionLibrary.cpp
+++ warrior/Private/WarriorFunctionLibrary.cpp
@@ -90,50 +90,6 @@ bool UWarriorFunctionLibrary::IsTargetPawnHostile(APawn* QueryPawn, APawn* Targe
 	return false;
 }
 
-FGameplayTag UWarriorFunctionLibrary::ComputeHitReactDirectionTag(const AActor* InAttacker, AActor* InVictim,
-	float& OutAngleDifference)
-{
-	check(InAttacker && InVictim);
-	
-	//获取前向向量和二者之间的单位方向向量
-	const FVector VictimForward=InVictim->GetActorForwardVector();
-	const FVector VictimToAttackerNormalized=(InAttacker->GetActorLocation()-InVictim->GetActorLocation()).GetSafeNormal();
-
-	//计算弧度值并转化为角度
-	const float DotResult=FVector::DotProduct(VictimForward,VictimToAttackerNormalized);
-	OutAngleDifference=UKismetMathLibrary::DegAcos(DotResult);
-	
-	//获得叉积
-	const FVector CrossResult=FVector::CrossProduct(VictimForward,VictimToAttackerNormalized);
-	//点积的z轴决定了角度值的正负（根据点积右手定则，垂直向量的方向正负就是二者角度的正负取值）
-	if (CrossResult.Z<0.f)
-	{
-		OutAngleDifference*=-1.f;
-	}
-	//前方
-	if (OutAngleDifference>=-45.f && OutAngleDifference<=45.f)
-	{
-		return WarriorGamePlayTags::Shared_Status_HitReact_Front;
-	}
-	//右方
-	if (OutAngleDifference>45.f && OutAngleDifference<=135.f)
-	{
-		return WarriorGamePlayTags::Shared_Status_HitReact_Right;
-	}
-	//左方
-	if (OutAngleDifference>135.f || OutAngleDifference<=-135.f)
-	{
-		return WarriorGamePlayTags::Shared_Status_HitReact_Back;
-	}
-	//后方
-	if (OutAngleDifference<-45.f && OutAngleDifference>-135.f)
-	{
-		return WarriorGamePlayTags::Shared_Status_HitReact_Left;
-	}
-
-	return WarriorGamePlayTags::Shared_Status_HitReact_Front;
-}
-
 bool UWarriorFunctionLibrary::IsValidBlock(AActor* InAttacker, AActor* InDefender)
 {
 	check(InAttacker && InDefender);
